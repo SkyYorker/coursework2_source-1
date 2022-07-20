4c6306a6ece242593682f1a0d21dfb947ca792c5
SkyYorker <48123293+SkyYorker@users.noreply.github.com>
2022-07-20 14:55:46 +0300

    Add files via upload

---
+++ templates/posts_dao.py
@@ -0,0 +1,61 @@
+import json
+
+
+
+class PostsDAO:
+
+
+    def __init__(self, path):
+        self.path = path
+
+
+    def load_json_data(self):
+        with open(f'{self.path}', 'r', encoding='utf-8') as file:
+            data_json = json.load(file)
+            return data_json
+
+    def get_posts_all(self):
+        return self.load_json_data()
+
+
+    def get_posts_by_user(self, user_name):
+        posts = self.get_posts_all()
+        posts_by_user = []
+
+        for user in posts:
+            if user['poster_name'] == user_name:
+                posts_by_user.append(user)
+        return posts_by_user
+            
+
+    def get_comments_by_post_id(self, post_id):
+        posts = self.get_posts_all()
+
+        for post in posts:
+            if post['pk'] == post_id:
+                return post
+
+
+
+    def search_for_posts(self, query):
+        posts = self.get_posts_all()
+        posts_list = []
+
+        query_lower = query.lower()
+
+        for post in posts:
+            if query_lower in post['content'].lower():
+                posts_list.append(post)
+        return posts_list
+
+
+    def get_post_by_pk(self, pk):
+        posts = self.get_posts_all()
+        
+        for post in posts:
+            if post['pk'] == pk:
+                return post
+
+
+    
+    
